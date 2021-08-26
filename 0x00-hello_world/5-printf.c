cfc96c87bd9dcbf949b35b7e695e57a17d73f2ba
eddah73 <eddahsjr@gmail.com>
2021-08-26 12:50:56 -0700

    use of prinf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - strings printed by printf
+ * followed by newline
+ * Return: alway 0 on (success)
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
