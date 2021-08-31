ed7561a9cdc607e3882be754deeef341919cb978
eddah73 <eddahsjr@gmail.com>
2021-08-31 15:17:18 -0700

    print holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,21 +1,19 @@
-#include "holberton.h"
-
-/**
- * main -  prints Holberton, followed by a new line
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-_putchar('H');
-_putchar('o');
-_putchar('l');
-_putchar('b');
-_putchar('e');
-_putchar('r');
-_putchar('t');
-_putchar('o');
-_putchar('n');
-_putchar('\n');
-
-return (0);
+#include "main.h"                                                                                                                                                         
+                                                                                                                                                                          
+/**                                                                                                                                                                       
+ * main - print Holberton                                                                                                                                                 
+ * follow by new line                                                                                                                                                     
+ * Return: always (0)                                                                                                                                                     
+ */                                                                                                                                                                       
+                                                                                                                                                                          
+int main(void)                                                                                                                                                            
+{                                                                                                                                                                         
+char c[10] = "Holberton";                                                                                                                                                 
+int i;                                                                                                                                                                    
+for (i = 0; i < 10; i++)                                                                                                                                                  
+{                                                                                                                                                                         
+_putchar(c[i]);                                                                                                                                                           
+}                                                                                                                                                                         
+_putchar('\n');                                                                                                                                                           
+return (0);                                                                                                                                                               
 }
