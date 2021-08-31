7c96d2292fcf6a94afc577d321036d73457d6bc2
eddah73 <eddahsjr@gmail.com>
2021-08-31 15:37:14 -0700

    print holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,19 +1,19 @@
-#include "main.h"                                                                                                                                                         
-                                                                                                                                                                          
-/**                                                                                                                                                                       
- * main - print Holberton                                                                                                                                                 
- * follow by new line                                                                                                                                                     
- * Return: always (0)                                                                                                                                                     
- */                                                                                                                                                                       
-                                                                                                                                                                          
-int main(void)                                                                                                                                                            
-{                                                                                                                                                                         
-char c[10] = "Holberton";                                                                                                                                                 
-int i;                                                                                                                                                                    
-for (i = 0; i < 10; i++)                                                                                                                                                  
-{                                                                                                                                                                         
-_putchar(c[i]);                                                                                                                                                           
-}                                                                                                                                                                         
-_putchar('\n');                                                                                                                                                           
-return (0);                                                                                                                                                               
+#include "main.h"
+
+/**
+ * main - print Holberton
+ * follow by new line
+ * Return: always (0)
+ */
+
+int main(void)
+{
+char c[10] = "Holberton";
+int i;
+for (i = 0; i < 10; i++)
+{
+_putchar(c[i]);
+}
+_putchar('\n');
+return (0);
 }
