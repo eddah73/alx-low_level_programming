d16c44d3004d7530d6f9ec034171d0e5317736bd
eddah73 <eddahsjr@gmail.com>
2021-09-21 15:03:58 -0700

    malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _strlen - returns the length of a string
  * @s: string s
- 
  * Return: length of string
  */
 int _strlen(char *s)
