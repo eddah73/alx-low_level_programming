d61acbfe3e78f9e14b32ee606badf677cbf62c1f
eddah73 <eddahsjr@gmail.com>
2021-09-30 00:59:39 -0700

    functions pointers

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,22 +1,50 @@
+#include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-/*addition*/
+/**
+ * op_add - adds a and b
+ * @a: first number
+ * @b: second number
+ *
+ * Return: sum of a and b
+ */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
-/*substraction*/
+
+/**
+ * op_sub - subtracts b from a
+ * @a: first number
+ * @b: second number
+ *
+ * Return: difference of a and b
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
-/*multiplication*/
+
+/**
+ * op_mul - multiplies a and b
+ * @a: first number
+ * @b: second number
+ *
+ * Return: multiplication of a and b
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
-/*division*/
+
+/**
+ * op_div - divides a by b
+ * @a: first number
+ * @b: second number
+ *
+ * Return: integer division of a by b
+ */
 int op_div(int a, int b)
 {
 	if (b == 0)
@@ -26,10 +54,16 @@ int op_div(int a, int b)
 	}
 	return (a / b);
 }
-/*modulo*/
+
+/**
+ * op_mod - find the remainder of the division of a by b
+ * @a: first number
+ * @b: second number
+ *
+ * Return: remainder of the division of a by b
+ */
 int op_mod(int a, int b)
 {
-
 	if (b == 0)
 	{
 		printf("Error\n");
@@ -37,5 +71,4 @@ int op_mod(int a, int b)
 	}
 	return (a % b);
 }
-
 
